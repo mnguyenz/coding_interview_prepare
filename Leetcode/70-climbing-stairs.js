bc44d69a411e14d64ea76642d3624488cdba2b2b
Minh Nguyen <mnguyen2606@gmail.com>
2022-12-12 09:31:31 +0700

    Daily and Basic SQL

---
+++ Leetcode/70-climbing-stairs.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const climbStairs = (n) => {
+  let arr = [1,2];
+  for (let i = 2; i < n; i++) {
+    arr[i] = arr[i-1] + arr[i-2];
+  }
+  return arr[n-1];
+};
+
+console.log(climbStairs(5));
