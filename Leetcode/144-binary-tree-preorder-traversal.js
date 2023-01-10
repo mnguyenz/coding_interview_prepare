b0bc74a2ea9f2a849881586c8c52eee3659d7730
Minh Nguyen <mnguyen2606@gmail.com>
2023-01-10 11:14:37 +0700

    Daily

---
+++ Leetcode/144-binary-tree-preorder-traversal.js
@@ -0,0 +1,21 @@
+function TreeNode(val, left, right) {
+  this.val = (val===undefined ? 0 : val)
+  this.left = (left===undefined ? null : left)
+  this.right = (right===undefined ? null : right)
+}
+
+var preorderTraversal = function(root) {
+  if (!root) return [];
+  var result = [];
+  var stack = [root];
+  
+  while(stack.length) {
+    var node = stack.pop();
+    result.push(node.val);
+    if (node.right) stack.push(node.right);
+    if (node.left) stack.push(node.left);
+  }
+  return result;
+};
+
+console.log(preorderTraversal([1,null,2,3]));
